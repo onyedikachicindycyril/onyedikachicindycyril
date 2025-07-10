6f9f64f69b82ddd086ea4daa20a7708c057a5094
chinemerem2525 <chubexchange@gmail.com>
2025-07-10 22:14:59 +0100

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cindy-cybersecurity-portfolio';
+constructor(private meta: Meta, private title: Title) {
+  this.title.setTitle('Onyedikachi – Certified Cyber Security Professional | Network Security | Incident Response | Risk Management');
+
+  this.meta.addTags([
+    {
+      name: 'description',
+      content:
+        'Hire Onyedikachi – a certified Cyber Security Professional with expertise in network security, vulnerability assessments, incident response, and risk management. Certified in CompTIA Security+, Tech+, and Digital Literacy Pro. Helping businesses protect digital assets and reduce cyber threats.'
+    },
+    {
+      name: 'keywords',
+      content:
+        'Cyber Security Professional, Network Security, Incident Response, Risk Management, Vulnerability Assessment, Cybersecurity Specialist, CompTIA Security+, Tech+ Certified, Security Awareness Training, Cybersecurity Nigeria, Digital Risk Consulting, Phishing Prevention, Security Best Practices, IT Compliance, Digital Forensics'
+    },
+    { name: 'author', content: 'Onyedikachi' },
+    {
+      property: 'og:title',
+      content: 'Onyedikachi – Certified Cyber Security Professional for Hire'
+    },
+    {
+      property: 'og:description',
+      content:
+        'Work with Onyedikachi – a trusted cybersecurity expert skilled in safeguarding networks, managing incidents, and assessing security risks. Proven experience delivering security awareness training and building cyber-resilient organizations.'
+    },
+    {
+      property: 'og:image',
+      content: 'https://onyedikachicindycyril.github.io/onyedikachicindycyril/assets/images/logo/logo1.png' 
+    },
+    { property: 'og:url', content: 'https://onyedikachicindycyril.github.io/onyedikachicindycyril' }
+  ]);
+}
+
 }
